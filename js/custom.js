8443072dbb239e49004ad1917a71bff4c9171181
ssuunnwwoooo <o3olia@naver.com>
2023-07-18 18:11:01 +0900

    230718

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,25 @@
-const MainSlide = new Swiper(".MainSlide", {
+
+const MainSlide = new Swiper('.MainSlide', {
+    loop: true,
     pagination: {
         el: ".swiper-pagination",
-        dynamicBullets: true,
+        clickable: true,
     },
+    autoplay: {
+        delay: 4000,
+    }
 });
+
+const MediSlide = new Swiper('.MediSlide', {
+    loop: true,
+    effect: 'fade',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+    },
+})
+
+
