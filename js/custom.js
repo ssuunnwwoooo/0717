7c927a914c1177b272f0ecd14420a5d1304513e8
ssuunnwwoooo <o3olia@naver.com>
2023-07-20 18:14:02 +0900

    230720

--- js/custom.js
+++ js/custom.js
@@ -21,17 +21,4 @@ const MediSlide = new Swiper('.MediSlide', {
             return '<span class="' + className + '">' + (index + 1) + "</span>";
         },
     },
-})
-
-
-$(document).ready(function () {
-    $('ul.tabs li').click(function () {
-        var tab_id = $(this).attr('data-tab');
-
-        $('ul.tabs li').removeClass('current');
-        $('.tab-content').removeClass('current');
-
-        $(this).addClass('current');
-        $("#" + tab_id).addClass('current');
-    });
-})
+});
