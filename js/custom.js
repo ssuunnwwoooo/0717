2400d237e9da9941fdda6fe4965d32015c163809
ssuunnwwoooo <o3olia@naver.com>
2023-07-21 13:12:30 +0900

    230721

--- js/custom.js
+++ js/custom.js
@@ -22,3 +22,29 @@ const MediSlide = new Swiper('.MediSlide', {
         },
     },
 });
+
+$(function () {
+    $('.gnb>ul>li>a').on('click', function (e) {
+        if ($('.gnb').hadClass('on')) {
+            e.preventDefault();
+        }
+        $(this).next().stop().slideDown();
+        $(this).parent().siblings().find('.sub').stop().slideUp();
+    });
+
+    $(window).on('resize', function () {
+        $('.gnb').removeClass('on')
+    });
+
+    $('.gnb').on('wheel', function (e) {
+        if ($('.gnb').hasClass('on')) {
+            e.preventDefault();
+        }
+    });
+
+    $('.mopen').on('click', function () {
+        $(this).toggleClass('on');
+        $('.gnb').toggleClass('on');
+    });
+})
+
